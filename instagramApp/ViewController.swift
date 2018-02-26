12ffc1ee11098939bf4081ead51b64219de5496b
Jigyasaa Sood <jsood@ucsd.edu>
2018-02-25 20:16:27 -0800

    Initial Commit

---
+++ instagramApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  instagramApp
+//
+//  Created by jsood on 2/25/18.
+//  Copyright © 2018 Jigyasaa Sood. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
